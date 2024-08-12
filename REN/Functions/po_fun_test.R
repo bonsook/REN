68c665967f6db8839ff066b06f5a968ab9ab1ca7
SHIJIA WANG <swan0278@student.monash.edu>
2024-08-12 17:14:12 +1000

    DS_Store as ignore file, added function to visuallize data

---
+++ REN/Functions/po_fun_test.R
@@ -0,0 +1,344 @@
+insert.at <- function(a, pos, ...){
+  dots <- list(...)
+  stopifnot(length(dots)==length(pos))
+  result <- vector("list",2*length(pos)+1)
+  result[c(TRUE,FALSE)] <- split(a, cumsum(seq_along(a) %in% (pos+1)))
+  result[c(FALSE,TRUE)] <- dots
+  result <- unlist(result)
+  if (pos == 0){
+    result <- unlist(c(dots,a))
+  }
+  if (pos == length(a)){
+    result <- unlist(c(a,dots))
+  }
+  return(result)
+}
+
+po.avg <- function (y0, x0, method = "LASSO"){
+  p0 <- dim(x0)[2]
+  j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+  x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+  p <- dim(x0)[2]
+  w <- foreach (i=1:p, .combine = rbind, .packages='glmnet', .export='insert.at') %dopar% {
+    y_tmp <- x0[,i] - y0
+    x_tmp <- x0[,i] - x0[,-i]
+    switch(method,
+           LASSO = {cv <- cv.glmnet(x_tmp,y_tmp,paralle = TRUE)
+           w_tmp <- cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+           insert.at(w_tmp,i-1,1-sum(as.numeric(w_tmp)))},
+           
+           RIDGE = {cv <- cv.glmnet(x_tmp,y_tmp,alpha = 0,paralle = TRUE)
+           w_tmp <- cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+           insert.at(w_tmp,i-1,1-sum(as.numeric(w_tmp)))},
+           
+           EN = {a <- seq(0.1, 0.9, 0.1)
+           search <- foreach(m = a, .combine = rbind) %dopar% {
+             cv <- cv.glmnet(x_tmp,y_tmp,alpha = m)
+             data.frame(cvm = cv$cvm[cv$lambda == cv$lambda.min], lambda.1se = cv$lambda.min, alpha = m)}
+           cv <- search[search$cvm == min(search$cvm), ]
+           w_tmp <- as.matrix(glmnet(x_tmp,y_tmp, lambda = cv$lambda.min, alpha = cv$alpha)$beta)
+           insert.at(w_tmp,i-1,1-sum(as.numeric(w_tmp)))})
+  }
+  w <- colMeans(w)
+  if (length(w) != p0) {
+    w <- insert.at(w,j-1,0)
+  }
+  return(w)}
+
+po.grossExp <- function (y0, x0, method = "NOSHORT"){
+  p0 <- dim(x0)[2]
+  j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+  x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+  p <- dim(x0)[2]
+  switch(method,
+         NOSHORT = {Dmat <- matrix(nearPD(cov(x0))["mat"][[1]]@x,p,p)   # cov(x0)  
+         dvec <- as.matrix(rep(0, p))
+         A.ge <- diag(p)
+         b.ge <- as.matrix(rep(0, p))
+         A.eq <- as.matrix(rep(1, p))
+         b.eq <- 1
+         sol  <- solve.QP(Dmat,dvec,Amat=cbind(A.ge,A.eq),bvec=c(b.ge,b.eq))$solution
+         if (any(round(sol,5) == 1)){
+           k <- which(round(sol,5)==1)
+           y_tmp <- x0[,k] - y0
+           x_tmp <- x0[,k] - x0[,-k]
+           cv <- cv.glmnet(x_tmp,y_tmp,paralle = TRUE)
+           w <- cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+           w <- insert.at(w,k-1,1-sum(as.numeric(w)))
+         } else {
+           y_tmp <- x0%*%sol - y0
+           x_tmp <- as.numeric(x0%*%sol) - x0
+           cv <- cv.glmnet(x_tmp,y_tmp,paralle = TRUE)
+           w <- cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+           w <- (1-sum(cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]))*sol + cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+         }},
+         
+         EQUAL = {y_tmp <- rowMeans(x0) - y0
+         x_tmp <- rowMeans(x0) - x0
+         cv <- cv.glmnet(x_tmp,y_tmp)
+         w <- (1-sum(cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]))*rep(1/p,p) + cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]})
+  if (length(w) != p0) {
+    w <- insert.at(w,j-1,0)
+  }
+  return(w)
+}
+
+po.covShrink <- function (y0, x0){
+  p0 <- dim(x0)[2]
+  j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+  x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+  p <- dim(x0)[2]
+  Dmat <- matrix(nearPD(cov_shrink(cov(x0)))["mat"][[1]]@x,p,p) #cov(x0)
+  dvec <- as.matrix(rep(0, p))
+  A.eq <- as.matrix(rep(1, p))
+  b.eq <- 1
+  w  <- solve.QP(Dmat,dvec,Amat=A.eq,bvec=b.eq)$solution
+  if (length(w) != p0) {
+    w <- insert.at(w,j-1,0)
+  }
+  return(w)
+}
+
+po.cols <- function (y0, x0){
+  p0 <- dim(x0)[2]
+  j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+  x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+  y_tmp <- x0[,1] - y0
+  x_tmp <- x0[,1] - x0[,-1]
+  w_tmp <- lm(y_tmp~x_tmp -1)$coefficients
+  w <- insert.at(w_tmp,0,1-sum(as.numeric(w_tmp)))
+  if (length(w) != p0) {
+    w <- insert.at(w,j-1,0)
+  }
+  return(w)
+}
+
+po.JM <- function (x0){
+  p0 <- dim(x0)[2]
+  j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+  x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+  p <- dim(x0)[2]
+  Dmat <- matrix(nearPD(cov(x0))["mat"][[1]]@x,p,p)  #  cov(x0)
+  dvec <- as.matrix(rep(0, p))
+  A.ge <- diag(p)
+  b.ge <- as.matrix(rep(0, p))
+  A.eq <- as.matrix(rep(1, p))
+  b.eq <- 1
+  w  <- solve.QP(Dmat,dvec,Amat=cbind(A.ge,A.eq),bvec=c(b.ge,b.eq))$solution
+  if (length(w) != p0) {
+    w <- insert.at(w,j-1,0)
+  }
+  return(w)
+}
+
+buh.clust <- function (x){
+  p0 <- dim(x)[2]
+  j <- which(apply(x, 2, var) <= 0, arr.ind = TRUE)
+  x <- x[,which(apply(x, 2, var) > 0, arr.ind = TRUE)]
+  p <- dim(x)[2]
+  CorMatix <- cor(x)-diag(p)
+  index <- seq(1,p,1)
+  group <- list()
+  b <- 0
+  for (i in (1:p)) {
+    group[[i]] <- index[i]
+  }
+  rho <- rep(NA,p-1)
+  tau <- list()
+  while (b <= (p-2)) {
+    b <- b+1
+    rho.max <- max(CorMatix)
+    rho[b] <- rho.max
+    tau[[b]] <- group
+    new.clust <- unique(as.numeric(which(CorMatix==max(CorMatix), arr.ind = T)))
+    group <- c(0,group)
+    group[[1]] <- c(group[[min(new.clust)+1]],group[[max(new.clust)+1]])
+    group[[min(new.clust)+1]] <- NULL
+    group[[max(new.clust)]] <- NULL
+    tmp <- rep(0,length(group))
+    for (i in (2:length(group))) {
+      if(length(group)<2) next
+      tmp[i] <- max(cancor(x[,group[[1]]],x[,group[[i]]])$`cor`)
+    }
+    CorMatix <- cbind(tmp, rbind(tmp[-1], CorMatix[-new.clust,-new.clust]))
+  }
+  group <- tau[[which.min(rho)]]
+  return(group)
+}
+
+# po.bhu <- function (y0, x0, group, rep){
+#   p0 <- dim(x0)[2]
+#   j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+#   x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+#   p <- dim(x0)[2]
+#   g <- length(group)
+#   x.bar <- foreach (i=1:g, .combine = cbind) %dopar% {
+#     if (length(group[[i]])>1){
+#       rowMeans(x0[,group[[i]]])
+#     } else {
+#       x0[,group[[i]]]
+#     }
+#   }
+#   Dmat <- matrix(nearPD(cov(x.bar))["mat"][[1]]@x,g,g) #cov(x0)
+#   dvec <- as.matrix(rep(0, g))
+#   A.ge <- diag(g)
+#   b.ge <- as.matrix(rep(0, g))
+#   A.eq <- as.matrix(rep(1, g))
+#   b.eq <- 1
+#   sol  <- round(solve.QP(Dmat,dvec,Amat=cbind(A.ge,A.eq),bvec=c(b.ge,b.eq))$solution,10)
+#   y_tmp <- x.bar%*%sol - y0
+#   x_tmp <- as.numeric(x.bar%*%sol) - x.bar
+#   lambda <- exp(seq(log(0.001), log(10), length.out=100))
+#   cv <- cv.glmnet(x_tmp,y_tmp,lambda=lambda)
+#   w.bar <- (1-sum(cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]))*sol + cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+#   p.bar <- length(which(w.bar!=0,arr.ind = T))
+#   w <- foreach (i=1:rep, .combine = rbind, .packages=c('glmnet','quadprog'), .export='insert.at') %dopar% {
+#         index <- which(w.bar!=0,arr.ind = T)
+#         for (k in (1:p.bar)){
+#             index[k] <- group[[index[k]]][sample(1:length(group[[index[k]]]), 1)]
+#         }
+#         Dmat <- cov(x0[,index])
+#         dvec <- as.matrix(rep(0, p.bar))
+#         A.eq <- as.matrix(rep(1, p.bar))
+#         b.eq <- 1
+#         sol  <- solve.QP(Dmat,dvec,Amat=A.eq,bvec=b.eq)$solution
+#         # construct gross exposure portfilio
+#         y_tmp <- x0[,index]%*%sol - y0
+#         x_tmp <- as.numeric(x0[,index]%*%sol) - x0
+#         cv <- cv.glmnet(x_tmp,y_tmp,paralle = TRUE)
+#         w <- cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+#         w[index] <- w[index] + (1-sum(cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]))*sol
+#         w
+#       }
+#       w <- colMeans(w)
+#   if (length(w) != p0) {
+#     w <- insert.at(w,j-1,0)
+#   }
+#   return(w)
+# }
+
+po.bhu <- function (y0, x0, group, rep){
+  p0 <- dim(x0)[2]
+  j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+  x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+  #p <- dim(x0)[2]
+  w <- foreach (i=1:rep, .combine = rbind, .packages=c('glmnet','quadprog'), .export='insert.at') %dopar% {
+    ind <- rep(NA,length(group))
+    for (k in (1:length(group))){
+      ind[k] <- group[[k]][sample(1:length(group[[k]]), 1)]
+    }
+    Dmat <- cov(x0[,ind])
+    dvec <- as.matrix(rep(0, length(group)))
+    A.ge <- diag(length(group))
+    b.ge <- as.matrix(rep(0, length(group)))
+    A.eq <- as.matrix(rep(1, length(group)))
+    b.eq <- 1
+    sol <- solve.QP(Dmat,dvec,Amat=cbind(A.ge,A.eq),bvec=c(b.ge,b.eq))$solution
+    if (any(round(sol,5) == 1)){
+      k <- which(round(sol,5)==1)
+      k <- ind[k]
+      y_tmp <- x0[,k] - y0
+      x_tmp <- x0[,k] - x0[,-k]
+      cv <- cv.glmnet(x_tmp,y_tmp,paralle = TRUE)
+      w.tmp <- cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+      insert.at(w.tmp,k-1,1-sum(as.numeric(w.tmp)))
+    } else {
+      y_tmp <- x0[,ind]%*%sol - y0
+      x_tmp <- as.numeric(x0[,ind]%*%sol) - x0
+      cv <- cv.glmnet(x_tmp,y_tmp,paralle = TRUE)
+      w.tmp <- cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+      w.tmp[ind] <- w.tmp[ind] + (1-as.numeric(sum(cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)])))*sol
+      w.tmp
+    }
+  }
+  w <- colMeans(w)
+  if (length(w) != p0) {
+    w <- insert.at(w,j-1,0)
+  }
+  return(w)
+}
+
+po.TZT <- function (x0, gamma){
+  p0 <- dim(x0)[2]
+  j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+  x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+  p <- dim(x0)[2]
+  t <- dim(x0)[1]
+  Dmat <- matrix(nearPD(cov(x0))["mat"][[1]]@x,p,p) #cov(x0)
+  dvec <- as.matrix(rep(0, p))
+  A.eq <- as.matrix(rep(1, p))
+  b.eq <- 1
+  w.mw <- solve.QP(Dmat,dvec,Amat=A.eq,bvec=b.eq)$solution
+  w.eq <- rep(1/p,p)
+  theta.sq <- colMeans(x0)%*%solve(Dmat)%*%colMeans(x0)
+  c1 <- ((t-2)*(t-p-2))/((t-p-1)*(t-p-4))
+  pi.1 <- w.eq%*%Dmat%*%w.eq - (2/gamma)*w.eq%*%colMeans(x0) + (1/gamma^2)*theta.sq
+  pi.2 <- (1/gamma^2)*(c1-1)*theta.sq + (c1/gamma^2)*(p/t)
+  delta <- pi.1/(pi.1+pi.2)
+  w <- (1-delta)%*%w.eq + delta%*%w.mw
+  if (length(w) != p0) {
+    w <- insert.at(w,j-1,0)
+  }
+  return(as.numeric(w))
+}
+
+po.SW <- function (x0, b, sample){
+  p <- dim(x0)[2]
+  j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+  x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+  p0 <- dim(x0)[2]
+  w <- foreach (i=1:sample, .combine = rbind, .packages=c('glmnet','quadprog'), .export='insert.at') %dopar% {
+    index <- sample(seq(1,p0,1), b, replace = FALSE)
+    Dmat <- cov(x0[,index])
+    dvec <- as.matrix(rep(0, b))
+    A.eq <- as.matrix(rep(1, b))
+    b.eq <- 1
+    sol  <- solve.QP(Dmat,dvec,Amat=A.eq,bvec=b.eq)$solution
+    w <- rep(0,p0)
+    w[index] <- sol
+    w
+  }
+  w <- colMeans(w)
+  if (length(w) != p) {
+    w <- insert.at(w,j-1,0)
+  }
+  return(as.numeric(w))
+}
+
+po.SW.lasso <- function (y0, x0, b, sample){
+  p <- dim(x0)[2]
+  j <- which(apply(x0, 2, var) <= 0, arr.ind = TRUE)
+  x0 <- x0[,which(apply(x0, 2, var) > 0, arr.ind = TRUE)]
+  p0 <- dim(x0)[2]
+  w <- foreach (i=1:sample, .combine = rbind, .packages=c('glmnet','quadprog'), .export='insert.at') %dopar% {
+    ind <- sample(seq(1,p0,1), b, replace = FALSE)
+    Dmat <- cov(x0[,ind])
+    dvec <- as.matrix(rep(0, b))
+    A.ge <- diag(b)
+    b.ge <- as.matrix(rep(0, b))
+    A.eq <- as.matrix(rep(1, b))
+    b.eq <- 1
+    sol  <- solve.QP(Dmat,dvec,Amat=cbind(A.ge,A.eq),bvec=c(b.ge,b.eq))$solution
+    if (any(round(sol,5) == 1)){
+      k <- which(round(sol,5)==1)
+      k <- ind[k]
+      y_tmp <- x0[,k] - y0
+      x_tmp <- x0[,k] - x0[,-k]
+      cv <- cv.glmnet(x_tmp,y_tmp,paralle = TRUE)
+      w.tmp <- cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+      insert.at(w.tmp,k-1,1-sum(as.numeric(w.tmp)))
+    } else {
+      y_tmp <- x0[,ind]%*%sol - y0
+      x_tmp <- as.numeric(x0[,ind]%*%sol) - x0
+      cv <- cv.glmnet(x_tmp,y_tmp,paralle = TRUE)
+      w.tmp <- cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)]
+      w.tmp[ind] <- w.tmp[ind] + (1-as.numeric(sum(cv$glmnet.fit$beta[,which(cv$lambda == cv$lambda.min)])))*sol
+      w.tmp
+    }
+  }
+  w <- colMeans(w)
+  if (length(w) != p) {
+    w <- insert.at(w,j-1,0)
+  }
+  return(as.numeric(w))
+}
---
+++ REN/R/Evaluation.R
@@ -0,0 +1,213 @@
+#' Perform Portfolio Optimization and Visualization
+#'
+#' This function performs portfolio optimization on a given dataset and generates visualizations
+#' for cumulative returns and turnover metrics.
+#'
+#' @param dat A pre-loaded data frame containing the dataset for analysis. The first column should contain the dates.
+#' @param date_column_index An integer indicating the index of the date column in the data frame. Default is 1.
+#' @param start_date A string representing the start date for filtering the data in 'YYYYMMDD' format. Default is '19990101'.
+#' @param end_date A string representing the end date for filtering the data in 'YYYYMMDD' format. Default is '20231231'.
+#' @param num_cores An integer specifying the number of CPU cores to use for parallel processing. Default is 7.
+#'
+#' @return A list containing cumulative return and turnover plots, as well as other calculated metrics.
+#' @import glmnet quadprog doParallel mvtnorm doSNOW lubridate CCA Matrix tictoc corpcor PerformanceAnalytics ggplot2 reshape2
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Example usage:
+#' data <- read.csv("path_to_your_data.csv")
+#' result <- perform_portfolio_optimization(dat = data, date_column_index = 1,
+#'                                          start_date = '19990101', end_date = '20231231', num_cores = 7)
+#' # Access the plots
+#' print(result$cumulative_return_plot)
+#' print(result$cumulative_turnover_plot)
+#' }
+perform_portfolio_optimization <- function(dat, date_column_index = 1, start_date = '19990101', end_date = '20231231', num_cores = 7) {
+
+  # Load necessary libraries
+  Packages <- c("glmnet", "quadprog", "doParallel", "mvtnorm", "doSNOW", "lubridate", "CCA", "Matrix", "tictoc", "corpcor", "PerformanceAnalytics", "ggplot2", "reshape2")
+  lapply(Packages, library, character.only = TRUE)
+  source("Functions/po_fun_test.R")  # Update the path accordingly
+
+  # Set up parallel processing
+  cl <- makeCluster(num_cores)
+  registerDoParallel(cl)
+
+  set.seed(1234)
+
+  # Prepare data
+  dat <- dat[,-date_column_index] # Remove the date column
+  dat <- matrix(unlist(dat), ncol = ncol(dat), byrow = "FALSE")
+  data <- dat[dat[,1] >= start_date & dat[,1] <= end_date, ]
+  Date <- as.Date(as.character(data[,1]), '%Y%m%d')
+  mon <- month(Date) + (year(Date) - year(Date)[1]) * 12
+  count <- as.numeric(table(mon))
+
+  x <- data[,2:ncol(dat)]
+  if (any(x == -99.99)) {
+    x <- x[, !apply(x, 2, function(col) any(col == -99.99))]
+  }
+
+  n <- dim(x)[1]
+  p <- dim(x)[2]
+
+  w.frame <- replicate(13, matrix(data = NA, nrow = max(mon) - 6, ncol = p))
+  stat.frame <- matrix(data = NA, nrow = max(mon) - 6, ncol = 13)
+
+  w <- w.frame
+  turn_over <- stat.frame
+
+  w0.tmp <- matrix(data = 1/p, nrow = p, ncol = 13)
+  r <- matrix(NA, sum(count[7:length(count)]), 13)
+
+  tic("total")
+
+  for (i in (7:max(mon))) {
+
+    w.tmp <- matrix(data = NA, nrow = p, ncol = 13)
+
+    x_tmp <- x[which((mon < i) & (mon >= i - 6)), ]
+    n_tmp <- dim(x_tmp)[1]
+    group <- buh.clust(x_tmp)
+
+    w.tmp[,1] <- po.cols(rep(0, n_tmp), x_tmp)                                                 # MV
+    w.tmp[,2] <- po.JM(x_tmp)                                                                 # JM
+    w.tmp[,3] <- po.avg(rep(0, n_tmp), x_tmp, method = "LASSO")                               # TRP_min
+    w.tmp[,4] <- po.avg(rep(0, n_tmp), x_tmp, method = "RIDGE")                               # APP-Ridge
+    w.tmp[,5] <- po.grossExp(rep(0, n_tmp), x_tmp, method = "NOSHORT")                        # Fan.et.al_JM
+    w.tmp[,6] <- po.grossExp(rep(0, n_tmp), x_tmp, method = "EQUAL")                           # FZY
+    w.tmp[,7] <- rep(1/p, p)                                                                   # EW
+    # w.tmp[,8] <- po.covShrink(rep(0, n_tmp), x_tmp)                                          # LW
+    w.tmp[,9] <- po.bhu(rep(0, n_tmp), x_tmp, group, 100)                                      # TRP_clu
+    w.tmp[,10] <- po.TZT(x_tmp, gamma = 3)                                                     # TZ
+    w.tmp[,11] <- po.SW(x_tmp, b = round(p^0.7), sample = 1000)                                # SW
+    w.tmp[,12] <- po.SW(x_tmp, b = round(length(group)), sample = 1000)                        # SW_clu
+    w.tmp[,13] <- po.SW.lasso(rep(0, n_tmp), x_tmp, b = round(length(group)), sample = 1000)   # SW_shrink
+
+    for (j in 1:13) {
+      w[i - 6,,j] <- w.tmp[,j]
+      turn_over[i - 6,j] <- norm(as.matrix(w.tmp[,j]) - w0.tmp[,j])
+    }
+
+    w0.tmp <- w.tmp
+
+    r_tmp <- (1 + (x[which(mon == i), ] / 100))
+
+    for (j in 1:13) {
+      for (k in 1:dim(r_tmp)[1]) {
+        idx <- which(mon == i)[k] - 124
+        if (idx > 0 && idx <= nrow(r)) {
+          r[idx,j] <- r_tmp[k,] %*% w0.tmp[,j]
+          w0.tmp[,j] <- (r_tmp[k,] * w0.tmp[,j]) / sum(r_tmp[k,] * w0.tmp[,j])
+        }
+      }
+      w0.tmp[,j] <- w0.tmp[,j] / sum(w0.tmp[,j])
+    }
+
+    print(i - 6)
+  }
+
+  stopCluster(cl)
+
+  toc()
+
+  ## Calculate Performance Metrics
+  colnames(turn_over) <- c("MV","JM","TRP_min","APP-Ridge","Fan.et.al_JM","FZY","EW","LW","TRP_clu","TZ","SW","SW_clu","SW_shrink")
+  turnover_mean <- colMeans(turn_over) * 100 # TO
+
+  sharpe_ratio <- sqrt(252) * (apply(r - 1, 2, mean) / apply(r - 1, 2, sd)) # SR
+
+  volatility <- sqrt(252) * apply(r - 1, 2, sd) * 100 # VO
+
+  v <- apply((1 + (r - 1)), 2, cumprod)
+  v1 <- as.matrix(apply(v, 2, max))
+  v1 <- matrix(rep(t(v1), dim(v)[1]), ncol = dim(v)[2], byrow = TRUE)
+  mdd <- (v1 - v) / v1 * 100
+  max_drawdown <- apply(mdd, 2, max)
+
+  ## VW Calculation
+  r_vw <- matrix(NA, sum(count[7:length(count)]), 1)
+  w_vw <- matrix(data = NA, nrow = max(mon) - 6, ncol = p)
+
+  vw0.tmp <- matrix(data = 1/p, nrow = p, ncol = 1)
+  vw.to <- matrix(data = NA, nrow = max(mon) - 6, ncol = 1)
+
+  for (i in 7:max(mon)) {
+    x_tmp <- x[which((mon < i) & (mon >= i - 6)), ]
+    n_tmp <- dim(x_tmp)[1]
+
+    r_tmp <- (1 + (x[which(mon == i), ] / 100))
+
+    for (k in 1:dim(r_tmp)[1]) {
+      idx <- which(mon == i)[k] - 124
+      if (idx > 0 && idx <= nrow(r_vw)) {
+        r_vw[idx] <- r_tmp[k,] %*% vw0.tmp
+        vw.tmp <- (r_tmp[k,] * vw0.tmp) / sum(r_tmp[k,] * vw0.tmp)
+      }
+    }
+    w_vw[i - 6, ] <- vw.tmp
+    vw.to[i - 6, ] <- norm(as.matrix(vw.tmp) - vw0.tmp)
+    vw0.tmp <- vw.tmp
+  }
+
+  vw_to_mean <- colMeans(vw.to) * 100 # TO
+  vw_sharpe_ratio <- sqrt(252) * (apply(r_vw - 1, 2, mean) / apply(r_vw - 1, 2, sd)) # SR
+  vw_volatility <- sqrt(252) * apply(r_vw - 1, 2, sd) * 100 # VO
+
+  vw_v <- apply((1 + (r_vw - 1)), 2, cumprod)
+  vw_v1 <- as.matrix(apply(vw_v, 2, max))
+  vw_v1 <- matrix(rep(t(vw_v1), dim(vw_v)[1]), ncol = dim(vw_v)[2], byrow = TRUE)
+  vw_mdd <- (vw_v1 - vw_v) / vw_v1 * 100
+  vw_max_drawdown <- apply(vw_mdd, 2, max)
+
+  ## Visualization
+  # Combine all the returns for visualization
+  r.final <- cbind(r, r_vw) # Combine all the returns of different methods
+  r.final <- r.final[, colSums(is.na(r.final)) == 0] # Remove empty columns
+
+  # Cumulative returns
+  cum.r <- apply(r.final - 1, 2, cumsum)
+  df <- data.frame(time = Date[-seq(1:sum(count[1:6]))], return = cum.r)
+  colnames(df) <- c("time", "MV", "JM", "FZY", "EW", "REN", "TZ", "VW")
+
+  df <- melt(df, id.vars = "time")
+  colnames(df) <- c("time", "Method", "value")
+
+  p1.ff25 <- ggplot(df, aes(x = time, y = value, color = Method)) + theme_bw() + geom_line() +
+    ggtitle("Cumulative Returns") +
+    scale_color_manual(values = c('red','cyan','magenta','grey47','black','lightblue',"green","orange","brown")) +
+    labs(y = "Cumulative Return", x = "Time") +
+    theme(legend.justification = c(0, 1), legend.position = c(0, 1)) + theme(legend.position = "bottom")
+
+  # Cumulative turnover
+  to.final <- cbind(turn_over, vw.to) # Combine all the TOs of different methods
+  to.final <- to.final[, colSums(is.na(to.final)) == 0] # Remove empty columns
+
+  index <- which(month(Date)[-1] != month(Date)[-length(month(Date))])
+
+  df2 <- data.frame(time = Date[index[6:length(index)]], turnover = apply(abs(to.final), 2, cumsum))
+  colnames(df2) <- c("time", "MV", "JM", "FZY", "EW", "REN", "TZ", "VW")
+
+  df2 <- melt(df2, id.vars = "time")
+  colnames(df2) <- c("time", "Method", "value")
+
+  p2.ff25 <- ggplot(df2, aes(x = time, y = value, color = Method)) + theme_bw() + geom_line() +
+    ggtitle("Cumulative Turnover") +
+    scale_color_manual(values = c('red','cyan','magenta','grey47','black','lightblue',"green","orange","brown")) +
+    labs(y = "Cumulative Turnover", x = "Time") +
+    theme(legend.justification = c(0, 1), legend.position = c(0, 1)) + theme(legend.position = "bottom")
+
+  return(list(
+    cumulative_return_plot = p1.ff25,
+    cumulative_turnover_plot = p2.ff25,
+    turnover_mean = turnover_mean,
+    sharpe_ratio = sharpe_ratio,
+    volatility = volatility,
+    max_drawdown = max_drawdown,
+    vw_to_mean = vw_to_mean,
+    vw_sharpe_ratio = vw_sharpe_ratio,
+    vw_volatility = vw_volatility,
+    vw_max_drawdown = vw_max_drawdown
+  ))
+}
